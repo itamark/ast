3cf006523694eb15f90277a7bd50f7f7067a9c85
Glitch (fascinated-marscapone) <none>
2020-01-04 22:59:20 +0000

    🎥🌛 Updated with Glitch

---
+++ app/app.jsx
@@ -0,0 +1,7 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+/* Import Components */
+const Home = require('./components/Home');
+
+ReactDOM.render(<Home/>, document.getElementById('main'));
